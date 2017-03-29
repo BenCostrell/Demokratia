39f8e39c13f6f9e21275cb34aaea3daf4c736e8f
Ben Costrell <bencostrell@gmail.com>
2017-03-29 14:25:50 -0400

    player initialization, structures

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    private List<Player> players;
+    public Vector3[] spawnPoints;
+    public Sprite[] playerSprites;
+
+	void Awake()
+    {
+        InitializeServices();
+    }
+    
+    // Use this for initialization
+	void Start () {
+        InitializePlayers();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Services.TaskManager.Update();
+	}
+
+    void InitializeServices()
+    {
+        Services.GameManager = this;
+        Services.EventManager = new EventManager();
+        Services.TaskManager = new TaskManager();
+        Services.PrefabDB = Resources.Load<PrefabDB>("Prefabs/PrefabDB");
+    }
+
+    void InitializePlayers()
+    {
+        players = new List<Player>();
+        for (int i = 0; i < 3; i++)
+        {
+            GameObject playerObj = Instantiate(Services.PrefabDB.Player, spawnPoints[i], Quaternion.identity) as GameObject;
+            players.Add(playerObj.GetComponent<Player>());
+            players[i].playerNum = i + 1;
+            players[i].gameObject.GetComponent<SpriteRenderer>().sprite = playerSprites[i];
+        }
+    }
+}
